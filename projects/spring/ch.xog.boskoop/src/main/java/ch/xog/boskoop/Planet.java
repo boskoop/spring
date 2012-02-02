0ccd4d1378599d3b04f6dd560e446cae4df6a0db
Christian Bürgi <mr.boskoop@gmail.com>
2012-02-02 02:55:23 +0100

    added javadoc

--- projects/spring/ch.xog.boskoop/src/main/java/ch/xog/boskoop/Planet.java
+++ projects/spring/ch.xog.boskoop/src/main/java/ch/xog/boskoop/Planet.java
@@ -1,9 +1,14 @@
 package ch.xog.boskoop;
 
+/**
+ * Another Spring bean.
+ * 
+ * @author buergich
+ */
 public class Planet {
-	
+
 	private String name;
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
